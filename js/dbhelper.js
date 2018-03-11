b5696bedbac84f995e487032bc31e8922deb82a4
baris_000 <barisozcetin@gmail.com>
2018-03-11 12:48:14 +0300

    changed port and img links

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 80 // Change this to your server port
+    const port = 5500 // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
@@ -143,14 +143,14 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return (`/restaurant.html?id=${restaurant.id}`);
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}`);
   }
 
   /**
